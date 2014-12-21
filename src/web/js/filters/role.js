45540fd6423dfb429e91dc6c9a582c1ce306a44e
Drew Stevens <wastevens@gmail.com>
2014-12-21 17:28:10 -0500

    Added support for distinguishing between filtering a single role vs a collection of roles.

--- src/web/js/filters/role.js
+++ src/web/js/filters/role.js
@@ -1,9 +1,14 @@
 angular.module('filters.role', ['sources.roles']).
 filter('role', ['rolesSource', function(rolesSource) {
 	return function(input) {
+		return rolesSource.get()[input];
+	};
+}]).
+filter('roles', ['$filter', function($filter) {
+	return function(input) {
 		var roleNames = [];
 		for(var i = 0;i<input.length;i++) {
-			roleNames[i] = rolesSource.get()[input[i]];
+			roleNames[i] = $filter('role')(input[i]);
 		}
 		return roleNames;
 	};
