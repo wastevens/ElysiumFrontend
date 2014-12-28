f216d5aadb6b7cb1891de19834ba763fae1459d8
Drew Stevens <wastevens@gmail.com>
2014-12-28 13:49:43 -0500

    Added setting to player character.  And support for ensuring default users are able to be easily re-created.

---
+++ src/resources/ElysiumFrontend-default-users.sql
@@ -0,0 +1,27 @@
+
+insert into User(id, email, password) values ('admin-id', 'admin@ut.org', 'password');
+insert into User(id, email, password) values ('lst-id', 'lst@ut.org', 'password');
+insert into User(id, email, password) values ('ast-id', 'ast@ut.org', 'password');
+insert into User(id, email, password) values ('narrator-id', 'narrator@ut.org', 'password');
+insert into User(id, email, password) values ('patron-id', 'patron@ut.org', 'password');
+insert into User(id, email, password) values ('user-id', 'user@ut.org', 'password');
+
+insert into User_roles(user_id, roles) values ('admin-id', 0);
+insert into User_roles(user_id, roles) values ('admin-id', 5);
+
+insert into User_roles(user_id, roles) values ('lst-id', 1);
+insert into User_roles(user_id, roles) values ('lst-id', 4);
+insert into User_roles(user_id, roles) values ('lst-id', 5);
+
+insert into User_roles(user_id, roles) values ('ast-id', 2);
+insert into User_roles(user_id, roles) values ('ast-id', 4);
+insert into User_roles(user_id, roles) values ('ast-id', 5);
+
+insert into User_roles(user_id, roles) values ('narrator-id', 3);
+insert into User_roles(user_id, roles) values ('narrator-id', 4);
+insert into User_roles(user_id, roles) values ('narrator-id', 5);
+
+insert into User_roles(user_id, roles) values ('patron-id', 4);
+insert into User_roles(user_id, roles) values ('patron-id', 5);
+
+insert into User_roles(user_id, roles) values ('user-id', 5);
