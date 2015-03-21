26080e5611705261ebf8b909903d16d6acfae26c
wastevens <wastevens@gmail.com>
2015-03-21 15:09:10 -0400

    Added stateless configuration.

--- src/java/com/dstevens/config/SecurityConfig.java
+++ src/java/com/dstevens/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import com.dstevens.users.ElysiumUserDetailsService;
  
@@ -26,10 +27,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	    http.
-	    	formLogin().loginPage("/login").successHandler(successHandler).failureUrl("/login?error").permitAll().and().
-	    	logout().logoutUrl("/logout").permitAll().and().
-	    	authorizeRequests().anyRequest().permitAll().and().
-		    csrf().disable(); 
+	         sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().
+	    	 formLogin().loginPage("/login").successHandler(successHandler).failureUrl("/login?error").permitAll().and().
+	    	 logout().logoutUrl("/logout").permitAll().and().
+	    	 authorizeRequests().anyRequest().permitAll().and().
+		     csrf().disable(); 
  
 	}
 }
