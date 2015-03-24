b3cce9967634216188d91cb64fb4b2938dc73049
wastevens <wastevens@gmail.com>
2015-03-24 18:52:42 -0400

    Fixed displayed user management to not list a bunch of dead roles.

--- src/web/js/sources/roles.js
+++ src/web/js/sources/roles.js
@@ -3,11 +3,7 @@ factory('rolesSource', [function() {
 	var map = [];
 	
 	map[0] = "Admin";
-	map[1] = "Lead Storyteller";
-	map[2] = "Assistant Storyteller";
-	map[3] = "Narrator";
-	map[4] = "Patron";
-	map[5] = "User";
+	map[1] = "User";
 	
 	return {
 		get: function() {
