62c8d18262cbf4e451396fb66f9c8f21dc83134d
wastevens <wastevens@gmail.com>
2015-03-30 14:55:22 -0400

    Added support for creating user with optional firstname, lastname, original username and payment identifier to createAccount.jsp

--- src/java/com/dstevens/web/account/controllers/CreateAccountController.java
+++ src/java/com/dstevens/web/account/controllers/CreateAccountController.java
@@ -1,5 +1,7 @@
 package com.dstevens.web.account.controllers;
-import static com.dstevens.collections.Sets.set;
+import java.time.Instant;
+import java.time.Year;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,20 +14,28 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.dstevens.mail.MailMessageFactory;
+import com.dstevens.suppliers.ClockSupplier;
 import com.dstevens.users.Role;
 import com.dstevens.users.User;
 import com.dstevens.users.UserDao;
+import com.dstevens.users.patronages.Patronage;
+import com.dstevens.users.patronages.PatronagePaymentReceipt;
+import com.dstevens.users.patronages.PaymentType;
+
+import static com.dstevens.collections.Sets.set;
  
 @Controller
 public class CreateAccountController {
 	
 	private UserDao userDao;
 	private MailMessageFactory messageFactory;
+	private ClockSupplier clockSupplier;
 
 	@Autowired
-	public CreateAccountController(UserDao userDao, MailMessageFactory messageFactory) {
+	public CreateAccountController(UserDao userDao, MailMessageFactory messageFactory, ClockSupplier clockSupplier) {
 		this.userDao = userDao;
 		this.messageFactory = messageFactory;
+		this.clockSupplier = clockSupplier;
 	}
 	
 	@RequestMapping(value = { "/createAccount"}, method = RequestMethod.GET)
@@ -35,16 +45,46 @@ public class CreateAccountController {
 	
 	@RequestMapping(value = { "/createAccount"}, method = RequestMethod.POST)
 	public ModelAndView createAccount(@RequestParam(value = "email") String email,
-			                          @RequestParam(value = "password") String password) {
+			                          @RequestParam(value = "password") String password,
+			                          @RequestParam(value = "firstName", required=false) String firstName,
+			                          @RequestParam(value = "lastName", required=false) String lastName,
+			                          @RequestParam(value = "originalUsername", required=false) String originalUsername,
+			                          @RequestParam(value = "paymentReceiptIdentifier", required=false) String paymentReceiptIdentifier) {
 		if(userDao.findWithEmail(email) != null) {
 			ModelAndView model = new ModelAndView("/account/createAccount");
 			model.addObject("error", "An account already exists for user with email address " + email);
 			return model;
 		}
-		User newUser = userDao.save(new User(email, password, set(Role.USER)));
+		User user = new User(email, password, set(Role.USER)).withFirstName(firstName).withLastName(lastName);
+		if(originalUsername != null && !originalUsername.trim().isEmpty()) {
+			Instant now = clockSupplier.get().instant();
+			user = user.withPatronage(new Patronage(Year.now(clockSupplier.get()).getValue(), Date.from(now), originalUsername));
+			if(paymentReceiptIdentifier != null && !paymentReceiptIdentifier.trim().isEmpty()) {
+				user.getPatronage().getPayments().add(new PatronagePaymentReceipt(PaymentType.PAYPAL, paymentReceiptIdentifier, Date.from(now)));
+			}
+		}
+		User newUser = userDao.save(user);
 		sendConfirmatoryEmailTo(email);
+		sendAdminEmailFor(newUser);
 		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(newUser, null, newUser.getAuthorities()));
 		return new ModelAndView(new RedirectView("/user/main"));
+	}
+
+	private void sendAdminEmailFor(User user) {
+		StringBuilder body = new StringBuilder();
+		body.append(user.getEmail() + " has just created an account on the database.");
+		if(user.getPatronage() != null) {
+			body.append("\nTheir original username in the old database is " + user.getPatronage().getOriginalUsername());
+			if(!user.getPatronage().getPayments().isEmpty()) {
+				body.append("\nTheir paypal receipt id for paying for patronage is " + user.getPatronage().getPayments().get(0).getPaymentReceiptIdentifier());
+			}
+		}
+		messageFactory.message().
+		               from("database@undergroundtheater.org", "UT Database").
+                       to("board@undergroundtheater.org").
+                       subject("A new Underground Theater User Account has been created").
+                       body(body.toString()).
+                       send();		
 	}
 
 	private void sendConfirmatoryEmailTo(String email) {
--- src/web/WEB-INF/pages/account/createAccount.jsp
+++ src/web/WEB-INF/pages/account/createAccount.jsp
@@ -28,10 +28,12 @@
 				<div style="color:red,font-style:bold">${error}</div>
 			</c:if>
 		    <table class="table">
-		    <tr><td>Email:</td><td><input type='text' name='email' value=''></td></tr>
-            <tr><td>Password:</td><td><input type='password' name='password' /></td></tr>
-            <tr><td>First Name:</td><td><input type='firstName' name='firstName' /></td></tr>
-            <tr><td>Last Name:</td><td><input type='lastName' name='lastName' /></td></tr>
+		    <tr><td>Email:</td><td><input type='text' name='email' required=true/></td></tr>
+            <tr><td>Password:</td><td><input type='password' name='password' required=true/></td></tr>
+            <tr><td>First Name (optional):</td><td><input type='firstName' name='firstName'/></td></tr>
+            <tr><td>Last Name (optional):</td><td><input type='lastName' name='lastName'/></td></tr>
+            <tr><td>Old Database Username (optional):</td><td><input type='text' name='originalUsername'/></td></tr>
+            <tr><td>Last Paypal Patronage Receipt Id (optional):</td><td><input type='text' name='paymentReceiptIdentifier'/></td></tr>
 			<tr><td>
                 <button class="btn btn-info" name="submit" type="submit" value="submit">
                     <span class="glyphicon glyphicon-save"></span> Submit
