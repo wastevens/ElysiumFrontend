57878497778daa86dcfab34b9247a7f2cee417dc
wastevens <wastevens@gmail.com>
2015-04-18 14:39:13 -0400

    Straightening up names.

--- src/java/com/dstevens/web/config/controllers/HelloController.java
+++ src/java/com/dstevens/web/config/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.dstevens.web.config.controller;
+package com.dstevens.web.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
