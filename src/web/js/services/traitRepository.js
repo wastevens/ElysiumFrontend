526d3002c9403b475e2341a7505adff6a86a1e82
wastevens <wastevens@gmail.com>
2015-04-29 21:16:27 -0400

    Moving towards pushing stuff into apis

--- src/web/js/services/traitRepository.js
+++ src/web/js/services/traitRepository.js
@@ -2,7 +2,7 @@ angular.module('services.traits', ['ngResource']).
 factory('traitsRepository', ['$resource', function($resource) {
 	return {
 		url: '/traits/',
-		getVampireTraits: function(traitType) {
+		getVampireTraits: function(traitType, parameters) {
 			var resource = $resource(this.url + "vampire/:traitType", {traitType: '@traitType'});
 			return resource.query({traitType: traitType}).$promise;
 		}
