6770bd4f6a9c2c3134bb94ba9d781f5eed7e5c08
wastevens <wastevens@gmail.com>
2015-06-09 10:03:29 -0400

    Added PropertySourcesPlaceholderConfigurer to read and populate properties; currently only loading app.properties (for persistence properties).

--- src/java/com/dstevens/config/AppConfig.java
+++ src/java/com/dstevens/config/AppConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -18,12 +20,18 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @Configuration
 @ComponentScan({ "com.dstevens.*" })
+@PropertySource("classpath:app.properties")
 @Import({ SecurityConfig.class })
 public class AppConfig extends WebMvcConfigurerAdapter {
  
 	@Autowired
 	private AuthorizationGuard tokenAuthorizationInterceptor;
 
+	@Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+	
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
