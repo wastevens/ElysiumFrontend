dbc1f72f5a99fa7995a362185b51e8274c457cb2
wastevens <wastevens@gmail.com>
2015-08-01 11:27:46 -0400

    Fixed a bug introduced by splitting the page and resource controllers

--- src/java/com/dstevens/web/admin/controllers/TroupePageController.java
+++ src/java/com/dstevens/web/admin/controllers/TroupePageController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 import com.dstevens.troupe.DisplayableTroupe;
 import com.dstevens.troupe.Troupe;
 import com.dstevens.troupe.TroupeRepository;
-import com.dstevens.troupe.UnknownTroupeException;
+import com.google.gson.Gson;
 
 @Controller
 public class TroupePageController {
@@ -30,11 +30,8 @@ public class TroupePageController {
 	@RequestMapping(value = "/admin/page/troupes/{id}", method = RequestMethod.GET)
 	public ModelAndView getManageTroupePage(@PathVariable Integer id) {
 		Troupe troupe = troupeRepository.findWithId(id);
-		if(troupe == null) {
-			throw new UnknownTroupeException("Could not find troupe with id " + id);
-		}
 		ModelAndView modelAndView = new ModelAndView("/admin/troupe/manage");
-		modelAndView.addObject("troupe", DisplayableTroupe.from(troupe));
+		modelAndView.addObject("troupe", new Gson().toJson(DisplayableTroupe.from(troupe)));
 		return modelAndView;
 	}
 }
