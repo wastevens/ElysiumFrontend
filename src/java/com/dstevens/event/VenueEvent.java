84920b592a05ca0b50671f93922a89ededf1d4f5
wastevens <wastevens@gmail.com>
2015-08-09 11:08:22 -0400

    Changed VenueEvent to carry PlayerCharacterOwnerships for attendance.

--- src/java/com/dstevens/event/VenueEvent.java
+++ src/java/com/dstevens/event/VenueEvent.java
@@ -3,20 +3,31 @@ package com.dstevens.event;
 import java.util.Date;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
-import com.dstevens.character.PlayerCharacter;
+import org.hibernate.annotations.ForeignKey;
+
 import com.dstevens.troupe.Venue;
+import com.dstevens.user.PlayerCharacterOwnership;
 
+@SuppressWarnings("deprecation")
 @Entity
-@DiscriminatorValue("VENUE")
 public class VenueEvent extends Event {
 
 	@Column(name="venue")
 	private final Venue venue;
 
+    @ManyToMany(cascade={CascadeType.ALL})
+	@JoinTable(name="TroupeEvent_Attendance", joinColumns = @JoinColumn(name="event_id"), 
+	           inverseJoinColumns = @JoinColumn(name="playerCharacterOwnership_id"))
+	@ForeignKey(name="TroupeEvent_PlayerCharacterOwnership_FK", inverseName="PlayerCharacterOwnership_TroupeEvent_FK")
+    private final Set<PlayerCharacterOwnership> attendance;
+	
     //Used only for hibernate
     @SuppressWarnings("unused")
 	@Deprecated
@@ -24,13 +35,18 @@ public class VenueEvent extends Event {
     	this(null, null, null, null, null, null);
     }
 	
-	public VenueEvent(Integer id, String name, Date eventDate, EventStatus eventStatus, Set<PlayerCharacter> attendingCharacters, Venue venue) {
-		super(id, name, eventDate, eventStatus, attendingCharacters);
+	public VenueEvent(Integer id, String name, Date eventDate, EventStatus eventStatus, Venue venue, Set<PlayerCharacterOwnership> attendance) {
+		super(id, name, eventDate, eventStatus);
 		this.venue = venue;
+		this.attendance = attendance;
 	}
 
 	public Venue getVenue() {
 		return venue;
+	}
+	
+	public Set<PlayerCharacterOwnership> getAttendance() {
+		return attendance;
 	}
 
 }
